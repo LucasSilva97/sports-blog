869cd2742406c318325a68fe7154e816001824cd
Lucas Santana <lucas-silva-santana@hotmail.com>
2023-05-06 09:25:57 -0300

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { FormulaOneNewsComponent } from './pages/formula-one-news/formula-one-ne
 import { VolleyballNewsComponent } from './pages/volleyball-news/volleyball-news.component';
 import { BasketballNewsComponent } from './pages/basketball-news/basketball-news.component';
 import { UfcNewsComponent } from './pages/ufc-news/ufc-news.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HighlightsCardComponent } from './components/highlights-card/highlights-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 
 @NgModule({
@@ -20,6 +25,11 @@ import { UfcNewsComponent } from './pages/ufc-news/ufc-news.component';
     VolleyballNewsComponent,
     BasketballNewsComponent,
     UfcNewsComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavBarComponent,
+    HighlightsCardComponent,
+    SmallCardComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/highlights-card/highlights-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlights-card',
+  templateUrl: './highlights-card.component.html',
+  styleUrls: ['./highlights-card.component.css']
+})
+export class HighlightsCardComponent {
+
+}
