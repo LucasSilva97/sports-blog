dfec69082f3c73c601bf632dd0a5af25510cf275
Lucas Santana <lucas-silva-santana@hotmail.com>
2023-05-07 01:34:53 -0300

    styling small cards

---
+++ src/app/data/fake_database.ts
@@ -0,0 +1,33 @@
+export const fake_database = [
+  {
+    "name":"Futebol",
+    "title":"",
+    "newsInitial":"",
+    "photoCover":""
+  },
+  {
+    "name":"Formula 1",
+    "title":"",
+    "newsInitial":"",
+    "photoCover":"",
+  },
+  {
+    "name":"Volei",
+    "title":"",
+    "newsInitial":"",
+    "photoCover":"",
+  },
+  {
+    "name":"Basquete",
+    "title":"",
+    "newsInitial":"",
+    "photoCover":"",
+  },
+  {
+    "name":"UFC",
+    "title":"",
+    "newsInitial":"",
+    "photoCover":"",
+  },
+]
+
