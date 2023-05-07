30ba87b2299aa47d7fbfb4cdbb7f0d8952d9015e
Lucas Santana <lucas-silva-santana@hotmail.com>
2023-05-07 12:40:15 -0300

    add data binding highlights

--- src/app/components/highlights-card/highlights-card.component.ts
+++ src/app/components/highlights-card/highlights-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-highlights-card',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./highlights-card.component.css']
 })
 export class HighlightsCardComponent {
-
+  @Input()
+  photoCover:string = ""
+  @Input()
+  newsTitle:string = ""
+  @Input()
+  newsDescription:string = ""
+  @Input()
+  name:string = ""
+  @Input()
+  dateNews:string = ""
 }
