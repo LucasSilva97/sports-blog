a18d0b72d63932d6f340072ea8b5af6d81c187ea
Lucas Santana <lucas-silva-santana@hotmail.com>
2023-05-07 16:39:31 -0300

    creating and styling content components

---
+++ src/app/data/fake_database.ts
@@ -0,0 +1,73 @@
+export const fake_database = [
+    {
+      "name":"highlights",
+      "photoCover":"https://paponacolina.com.br/wp-content/uploads/2023/05/1_luciano_juba_goat_sport-22912884.jpg",
+      "title":'"Luciano Juba pode deixar o Sport a partir de Agosto?"',
+      "description":`Destaque do Sport, Luciano Juba pode estar deixando a Ilha do Retiro para assinar com o São Paulo.
+      Ao que tudo indica, a diretoria do tricolor já vinha conversando com o empresário do jogador, e o meia-atacante
+      deve deixar o leão em Agosto desse ano, perto do final do 1º turno do Campeonato Brasileiro série B.
+      Os torcedores do leão da praça da bandeira lamentam pela saida do Juba, que é o destaque
+      do Sport desse ano, e vem trazendo uma baita temporada. Em entrevista, o jogador disse:
+      'Eu devo tudo ao Sport, e vou jogar até o último minuto enquanto vestir essa camisa pesada, desse grande clube que é o Sport.'`
+    },
+  {
+    "name":"highlight_one",
+    "photoCover":"https://classic.exame.com/wp-content/uploads/2023/04/GettyImages-1241251278.jpg?quality=70&strip=info&w=1024",
+    "title":'"De casa nova"',
+    "description":`O Tricolor Paulista é a nova casa do Técnico, experiente, Dorival Júnior,
+                  vai que vai respirar novos ares a pois a saída do então técnico do
+                  São Paulo, e ídolo, Rogério Ceni. Dorival terá o grande desafio de restaurar
+                  os bons tempos de bom futebol de um dos maiores times da história do futebol
+                  que está em crise a muito tempo. A muito trabalho a se fazer no tricolor
+                  do Morumbi.`,
+  },
+  {
+    "name":"highlight_two",
+    "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2022/07/shutterstock_2182983733.jpg",
+    "title":'"Red Bull te dá asaaas..."',
+    "description":`Desde 2021 até agora, o que vimos é o neerlandês Verstapen tomar
+                  a frente como destaque dos autôdromos ocupados pelos veículos de motores
+                  V6, que move vários apaixonados por motores a assistir esse evento todo
+                  final de semana, quase como um mantra.
+                  Esse posto até então era do britânico e multi-campeão Lewis Hamilton, que
+                  viu sua Mercedez sendo desbancada pelo motor Ford super potente da Red Bull.
+                  Antes, o inglês só precisava se preocupar com o Verstapen, mas hoje
+                  essa dor de cabeça dobrou, e agora o que vemos são as duas RBs liderando
+                  a atual temporada, e com louvor! Em entrevista exclusiva com o engenheiro
+                  master da Red Bull, o mesmo fala: "desde depois do Vettel, continuamos
+                  brigando ali pelo meio da tabela, não mais pelas pontas, vimos nosso veículo
+                  sendo literalmente ultrapassado pelos demais, e tivemos que fazer
+                  modicações severas quando a aerodinâmica, e em especial o motor. `,
+  },
+  {
+    "name":"highlight_three",
+    "photoCover":"https://conteudo.imguol.com.br/c/olimpiadas/e8/2016/07/25/wallace-jogador-dde-volei-do-brasil-comemora-ponto-na-liga-mundial-1469481651932_v2_600x800.jpg",
+    "title":'Entenda o caso do jogador de Vôlei',
+    "description":`Após ser pego no dopping, jogador poderá ficar em até 5 anos de suspensão,
+                  com 37 anos de idade, isso poderá resultar numa
+                  precoce aposentadoria, já que não estava nos seus planos. Autoridades informam
+                  que o caso ainda está em término de apuração, mas que até o mês de agosto
+                  seja dado o veredito, e dependendo do resultado, a possível punição
+                  cabível. `,
+  },
+  {
+    "name":"highlight_four",
+    "photoCover":"https://tatame.com.br/wp-content/uploads/2021/12/julianna-pena-cinturao-ufc-belt-mma.jpg",
+    "title":'Mais um confronto',
+    "description":`Amanda Nunes enfrenta pela 3ª vez a sua arquirrival Julianna Pena, num confroto histórico, em quase que uma
+                  trilogia pela disputa do cinturão. Na última luta, em 2022, Nunes levou a melhor, e agora vai com tudo
+                  em defesa do cinturão da categoria peso-pena.
+                  Amanda Nunes tem um placar total de 22 vitórias e cinco derrotas no UFC, e venceu 12 das 13 lutas
+                  mais recentes que teve. Já a Julina, tenta se superar, desbamcar a atual campeã do cinturão pela
+                  segunda vez seria um fato ainda inédito no mundo do MMA`,
+  },
+  {
+    "name":"highlight_five",
+    "photoCover":"https://www.cob.org.br/pt/img/noticia/imagem/4130/grande/",
+    "title":`Tókio 2024, é cesta!`,
+    "description":`Nossas jogadoras estão confiantes para a Olimpíadas do ano que vem, e vêm se preparando
+                  a todo vapor. O grupo aparenta estar muito unido, e também ambiocioso, pois o objetivo do time
+                  já está bem claro: MEDALHA DE OURO!`,
+  },
+
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {fake_database} from '../../data/fake_database';
 
 @Component({
   selector: 'app-content',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
 
+  constructor(private route:ActivatedRoute){
+
+  }
+
+  private name:string | null = ""
+
+  ngOnInit():void{
+    this.route.paramMap.subscribe(
+      value => this.name = value.get("name")
+    )
+
+    this.setValuesToComponent(this.name)
+  }
+
+  setValuesToComponent(name:string | null){
+    const data = fake_database.filter(article => article.name == name)[0]
+
+    this.photoCover = data.photoCover;
+    this.contentDescription = data.description;
+    this.contentTitle = data.title;
+  }
 }
