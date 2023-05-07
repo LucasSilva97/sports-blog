b6d887c026b734e944c0810de1589b4c50bd88e0
Lucas Santana <lucas-silva-santana@hotmail.com>
2023-05-07 11:21:46 -0300

    styling small cards

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
+
 export class SmallCardComponent {
   @Input()
   photoCover:string = ""
@@ -14,4 +16,7 @@ export class SmallCardComponent {
   newsDescription:string = ""
   @Input()
   name:string = ""
+  @Input()
+  dateNews:string = ""
+
 }
